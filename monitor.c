a80269827842871309bca7858ca13f67fb28848b
WingTo316 <wing0316@connect.hku.hk>
2021-10-10 20:08:05 +0800

    init

--- monitor.c
+++ monitor.c
@@ -4,18 +4,32 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]) {
 
-    if (argc == 1) { exit(0); } /* if no arg */
+    if (argc == 1) { exit(0); } // if no arg
 
-    if (argv[1][0] == '/') {
-        printf("it is /\n");
-    } else if (argv[1][0] == '.') {
-        printf("it is .\n");
-    } else {
-        printf("it is %s\n", argv[1]);
+    char** argVector = malloc(argc * sizeof *argVector);
+
+    for (int i=1; i<argc; i++) {
+        argVector[i-1] = strdup(argv[i]);
+    }
+    // argVector[argc] = NULL;
+
+    for (int i = 0; i < argc-1; i++) {
+        printf("%s\n", argVector[i]);
     }
 
     return 0;
+}
+
+char* get_progname(char* arg[]) {
+    if (arg[0] == '/') {
+        printf("it is /\n");
+    } else if (arg[0] == '.') {
+        printf("it is .\n");
+    } else {
+        printf("it is %s\n", arg[2]);
+    }
 }
