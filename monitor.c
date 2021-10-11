4a6ee921f6e83f7cc1dcb30e3c3210ea96ce39e3
WingTo316 <wing0316@connect.hku.hk>
2021-10-11 19:22:14 +0800

    without pipe

--- monitor.c
+++ monitor.c
@@ -5,6 +5,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/resource.h>
+#include <sys/time.h>
+
+pid_t start_process(char**, struct timeval*);
+
+void statistics(struct rusage, struct timeval, struct timeval);
 
 int main(int argc, char* argv[]) {
 
@@ -49,22 +57,53 @@ int main(int argc, char* argv[]) {
         tmp_len++;
     }
 
+    int* pipefd = (int*) malloc((num_of_cmd-1) * sizeof(int));
+    for (int i=0; i<num_of_cmd-1; i++) {
+        pipe(&pipefd[2*i]);
+    }
+
+    pid_t* pids = (pid_t*) malloc(num_of_cmd * sizeof(pid_t));
+    struct rusage state;
+    int wstatus;
+    struct timeval start, end;
     for (int i=0; i<num_of_cmd; i++) {
-        for (int j=0; j<cmd_len[i]; j++) {
-            printf("%s ", arg_vector[i][j]);
-        }
-        printf("\n");
+        pids[i] = start_process(arg_vector[i], &start);
+        printf("\nProcess with id: %d created for the command: %s\n", pids[i], arg_vector[i][0]);
+        wait4(pids[i], &wstatus, 0, &state);
+        gettimeofday(&end, NULL);
+        printf("The command \"%s\" terminated with returned status code = %d\n\n", arg_vector[i][0], WEXITSTATUS(wstatus));
+        statistics(state, start, end);
     }
 
     return 0;
 }
 
-char* get_progname(char* arg[]) {
-    if (arg[0] == '/') {
-        printf("it is /\n");
-    } else if (arg[0] == '.') {
-        printf("it is .\n");
-    } else {
-        printf("it is %s\n", arg[2]);
+pid_t start_process(char** arg, struct timeval* start) {
+    gettimeofday(start, NULL);
+    pid_t pid = fork();
+    if (pid == -1) {
+        printf("fork() error!\n");
+        exit(-1);
+    } else if (pid == 0) {
+        if (arg[0][0] == '/') {
+            execv(arg[0], arg);
+        } else {
+            execvp(arg[0], arg);
+        }
+        printf("exec: : No such file or directory\n");
+        printf("monitor experienced an error in starting the command: %s\n", arg[0]);
+        exit(-1);
     }
+    return pid;
+}
+
+void statistics(struct rusage usage, struct timeval start, struct timeval end) {
+    struct timeval real;
+    timersub(&end, &start, &real);
+    double r_sec = real.tv_sec + (double) real.tv_usec / 1000000.0;
+    double u_sec = usage.ru_utime.tv_sec + (double) usage.ru_utime.tv_usec / 1000000.0;
+    double s_sec = usage.ru_stime.tv_sec + (double) usage.ru_stime.tv_usec / 1000000.0;
+    printf("real: %.3f s, user: %.3f s, system: %.3f s\n", r_sec, u_sec, s_sec);
+    printf("no. of page faults: %ld\n", usage.ru_minflt + usage.ru_majflt);
+    printf("no. of context switches: %ld\n", usage.ru_nvcsw + usage.ru_nivcsw);
 }
