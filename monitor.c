48505a3073addc18ff544fbad883a5c2b5df68e1
WingTo316 <wing0316@connect.hku.hk>
2021-10-11 15:05:18 +0800

    store argv

--- monitor.c
+++ monitor.c
@@ -10,15 +10,50 @@ int main(int argc, char* argv[]) {
 
     if (argc == 1) { exit(0); } // if no arg
 
-    char** argVector = malloc(argc * sizeof *argVector);
+    int num_of_cmd = 1;
 
     for (int i=1; i<argc; i++) {
-        argVector[i-1] = strdup(argv[i]);
+        if (strcmp(argv[i],"!") == 0)
+            num_of_cmd++;
     }
-    // argVector[argc] = NULL;
 
-    for (int i = 0; i < argc-1; i++) {
-        printf("%s\n", argVector[i]);
+    int* cmd_len = (int*) malloc(num_of_cmd * sizeof(int));
+
+    int tmp_len = 0;
+    int index = 0;
+    for (int i=1; i<argc; i++) {
+        tmp_len++;
+        if (strcmp(argv[i],"!") == 0) {
+            cmd_len[index] = tmp_len - 1;
+            index++;
+            tmp_len = 0;
+        }
+        if (i == argc-1) {
+            cmd_len[index] = tmp_len;
+        }
+    }
+
+    char*** arg_vector = (char***) malloc(num_of_cmd * sizeof(char**));
+    for (int i=0; i<num_of_cmd; i++)
+        arg_vector[i] = (char**) malloc(num_of_cmd * sizeof(char*));
+
+    tmp_len = 0;
+    index = 0;
+    for (int i=1; i<argc; i++) {
+        if (strcmp(argv[i],"!") == 0) {
+            index++;
+            tmp_len = 0;
+            continue;
+        }
+        arg_vector[index][tmp_len] = strdup(argv[i]);
+        tmp_len++;
+    }
+
+    for (int i=0; i<num_of_cmd; i++) {
+        for (int j=0; j<cmd_len[i]; j++) {
+            printf("%s ", arg_vector[i][j]);
+        }
+        printf("\n");
     }
 
     return 0;
