190ca18552d7559527e0eead676d5bb9e1df699c
WingTo316 <wing0316@connect.hku.hk>
2021-10-12 14:12:03 +0800

    finish

--- monitor.c
+++ monitor.c
@@ -15,17 +15,18 @@ void sighandler(int);
 
 int main(int argc, char* argv[]) {
 
-    if (argc == 1) { exit(0); } // if no arg
+    if (argc == 1) { exit(0); } // exit if no arg
 
-    signal(SIGINT,sighandler);
+    signal(SIGINT,sighandler); // ignore SIGINT
 
+    // find the num of program to execute
     int num_of_cmd = 1;
-
     for (int i=1; i<argc; i++) {
         if (strcmp(argv[i],"!") == 0)
             num_of_cmd++;
     }
 
+    // find the num of argument of each program
     int* cmd_len = (int*) malloc(num_of_cmd * sizeof(int));
 
     int tmp_len = 0;
@@ -37,15 +38,16 @@ int main(int argc, char* argv[]) {
             index++;
             tmp_len = 0;
         }
-        if (i == argc-1) {
+        if (i == argc-1)
             cmd_len[index] = tmp_len;
-        }
     }
 
+    // 3D array store i-th program j-th argument at arg_vector[i][j]
     char*** arg_vector = (char***) malloc(num_of_cmd * sizeof(char**));
     for (int i=0; i<num_of_cmd; i++)
         arg_vector[i] = (char**) malloc(num_of_cmd * sizeof(char*));
 
+    // load argument to arg_vector[][][]
     tmp_len = 0;
     index = 0;
     for (int i=1; i<argc; i++) {
@@ -58,55 +60,60 @@ int main(int argc, char* argv[]) {
         tmp_len++;
     }
 
+    // init (num of program - 1) pair pipefd
     int* pipefd = (int*) malloc(2 * (num_of_cmd-1) * sizeof(int));
     for (int i=0; i<num_of_cmd-1; i++)
         pipe(&pipefd[2*i]);
 
-    pid_t pid;
-    struct rusage state;
-    int wstatus;
-    struct timeval start, end;
+    pid_t pid;                  // child PID
+    struct rusage state;        // resource usage of child
+    int wstatus;                // exit status of child
+    struct timeval start, end;  // start time & end time of child
 
     for (int i=0; i<num_of_cmd; i++) {
-        pid = pipe_process(arg_vector[i], &start, i, num_of_cmd, pipefd);
+
+        pid = pipe_process(arg_vector[i], &start, i, num_of_cmd, pipefd); // execute i-th program
         printf("\nProcess with id: %d created for the command: %s\n", pid, arg_vector[i][0]);
 
-        wait4(pid, &wstatus, 0, &state);
-        gettimeofday(&end, NULL);
+        wait4(pid, &wstatus, 0, &state); // wait for i-th finish
+        gettimeofday(&end, NULL); // find end time
 
-        if (WIFEXITED(wstatus))
+        if (WIFEXITED(wstatus)) // if child terminated normally
             printf("The command \"%s\" terminated with returned status code = %d\n\n", arg_vector[i][0], WEXITSTATUS(wstatus));
-        if (WIFSIGNALED(wstatus))
+        if (WIFSIGNALED(wstatus)) // if terminated by a signal
             printf("The command \"%s\" is interrupted by the signal number = %d (%s)\n\n",
                    arg_vector[i][0], WTERMSIG(wstatus), strsignal(WTERMSIG(wstatus)));
 
-        statistics(state, start, end);
+        statistics(state, start, end); // print statistics
     }
     return 0;
 }
 
 pid_t pipe_process(char** arg, struct timeval* start, int index, int num_of_cmd, int* pipefd) {
-    gettimeofday(start, NULL);
+    gettimeofday(start, NULL); // find start time
     pid_t pid = fork();
     if (pid == -1) {
-        printf("fork() error!\n");
+        printf("fork() error!");
         exit(-1);
-    } else if (pid == 0) {
-        if (index != 0)
+    } else if (pid == 0) { // child process
+
+        if (index != 0) // if not first program, read from pipe
             dup2(pipefd[2*index-2], 0);
-        if (index != num_of_cmd-1)
+        if (index != num_of_cmd-1) // if not last program, write to pipe
             dup2(pipefd[2*index+1], 1);
-        for (int i=0; i<2*(num_of_cmd-1); i++)
+        for (int i=0; i<2*(num_of_cmd-1); i++) // close pipe
             close(pipefd[i]);
-        if (arg[0][0] == '/') {
+
+        if (arg[0][0] == '/') // absolute path
             execv(arg[0], arg);
-        } else {
+        else // relative path
             execvp(arg[0], arg);
-        }
+
         printf("exec: : No such file or directory\n");
         printf("monitor experienced an error in starting the command: %s\n", arg[0]);
         exit(-1);
     }
+    // for father process, close used pipe end
     if (index != 0)
         close(pipefd[2*index-2]);
     if (index != num_of_cmd-1)
@@ -116,7 +123,7 @@ pid_t pipe_process(char** arg, struct timeval* start, int index, int num_of_cmd,
 
 void statistics(struct rusage usage, struct timeval start, struct timeval end) {
     struct timeval real;
-    timersub(&end, &start, &real);
+    timersub(&end, &start, &real); // calculate real time
     double r_sec = real.tv_sec + (double) real.tv_usec / 1000000.0;
     double u_sec = usage.ru_utime.tv_sec + (double) usage.ru_utime.tv_usec / 1000000.0;
     double s_sec = usage.ru_stime.tv_sec + (double) usage.ru_stime.tv_usec / 1000000.0;
@@ -125,4 +132,4 @@ void statistics(struct rusage usage, struct timeval start, struct timeval end) {
     printf("no. of context switches: %ld\n", usage.ru_nvcsw + usage.ru_nivcsw);
 }
 
-void sighandler(int signum) { return; }
+void sighandler(int signum) { return; } // ignore SIGINT
